2a0f5167f6feb63066d514ba50d94f4757021c4e
zzz <tenkanse@hotmail.com>
2019-07-22 18:02:21 +0800

    改批量导入

--- src/Command/FetchPostsCommand.php
+++ src/Command/FetchPostsCommand.php
@@ -4,6 +4,9 @@ namespace App\Command;
 
 use App\Entity\Post;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -20,6 +23,8 @@ class FetchPostsCommand extends Command
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
+        $this->batchSize = 20;
+        $this->maxLimit = 1000;
 
         parent::__construct();
     }
@@ -36,91 +41,115 @@ class FetchPostsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $cmd = "(sleep 60  && kill -9 ".getmypid().") > /dev/null &";
-        exec($cmd); //issue a command to force kill this process in 10 seconds
-        
+        //$cmd = "(sleep 60  && kill -9 ".getmypid().") > /dev/null &";
+        //exec($cmd); //issue a command to force kill this process in 10 seconds
+
         $this->io = new SymfonyStyle($input, $output);
         $keyword = $input->getArgument('keyword');
-        $page = $input->getArgument('page');
-        $maxLimit = $input->getArgument('max_limit');
+        $page = (int) $input->getArgument('page');
+        $maxLimit = (int) $input->getArgument('max_limit');
 
         if ($keyword) {
             $this->io->note(sprintf('搜索关键字: %s', $keyword));
         }
 
-        $currentPage = 0;
-        if ($page) {
-            $this->io->note(sprintf('初始页数: %s', $page));
-            $currentPage = (int) $page;
+        if ($maxLimit > 0) {
+            $this->maxLimit = min($this->maxLimit, $maxLimit);
         }
-        $resData = $this->fetchPage($keyword, $currentPage);
-        $count = 0;
+        $maxBatchCount = (int) $this->maxLimit/$this->batchSize;
 
-        if (isset($resData['data'])) {
-            $maxPage = $resData['pages'];
-            $this->saveToDb($resData['data']);
-            while ($currentPage < $maxPage) {
-                $count++;
-                if ($maxLimit && $count > $maxLimit) {
-                    break;
-                }
-                $currentPage++;
-                $this->io->note('开始导入第'.$currentPage.'页数据');
-                $resData = $this->fetchPage($keyword, $currentPage);
-                if (isset($resData['data']) && is_array($resData['data'])) {
-                    $this->saveToDb($resData['data']);
-                }
-            }
+        if ($page <= 1) {
+            $page = 1;
         }
+        $this->io->note(sprintf('初始页数: %s', $page));
+
+        for ($i = 0; $i <= $maxBatchCount; $i++) {
+            $startPage = $page + $i * $this->batchSize;
+            $endPage = $startPage + $this->batchSize;
+            $resData = $this->batchFetchPage($keyword, $startPage);
+            $this->io->note('开始导入第'.$startPage.'-'.$endPage.'页数据');
+            $this->saveToDb($resData);
+        }
+
         $this->io->success('成功导入');
     }
 
-    protected function fetchPage($keyword, $page)
+    protected function batchFetchPage($keyword, $page)
     {
-        $client = new \GuzzleHttp\Client();
-        $boardUrl = 'http://bbs.jjwxc.net/bbsapi.php?action=board';
-        $searchUrl = 'http://bbs.jjwxc.net/bbsapi.php?action=search';
-        $postData = array(
-            'board' => '2',
-            'page' => $page,
-            'sign' => 't1y30KJlifEX7XJeoSv3NvZifLl08tdwcBxJKi130qUIi1mJOpMGV7om4rii/AzhM3h3RhnMFS4%3D',
-            'source' => 'IOS',
-            'versionCode' => '195',
-        );
-        $url = $boardUrl;
+        $url = 'http://bbs.jjwxc.net/bbsapi.php?action=';
         if ($keyword) {
-            $postData['keyword'] = $keyword;
-            $postData['topic'] = '3';
-            $url = $searchUrl;
+            $url .= 'search';
+        } else {
+            $url .= 'board';
         }
-        $res = $client->request('POST', $url, array(
-            'form_params' => $postData,
-        ));
-        $res = $res->getBody();
-        $resData = json_decode($res, true);
-        // dump($resData);
-        return $resData ?? [];
+        $client = new Client();
+        $promises = array_map(function ($page) use ($client, $url, $keyword) {
+            $postData = array(
+                'board' => '2',
+                'page' => $page,
+                'sign' => 't1y30KJlifEX7XJeoSv3NvZifLl08tdwcBxJKi130qUIi1mJOpMGV7om4rii/AzhM3h3RhnMFS4%3D',
+                'source' => 'IOS',
+                'versionCode' => '195',
+                'keyword' => $keyword,
+                'topic' => 3,
+            );
+            return $client->postAsync($url, array(
+                'form_params' => $postData,
+            ));
+        }, range($page, $page + $this->batchSize - 1));
+        $results = Promise\unwrap($promises);
+        $results = array_reduce($results, function ($acc, $res) {
+            if (!$res instanceof \GuzzleHttp\Psr7\Response) {
+                return $acc;
+            }
+            $res = $res->getBody();
+            $resData = @json_decode($res, true);
+            $resData['data'] = $resData['data'] ?? [];
+            $acc = array_merge($resData['data'], $acc);
+            return $acc;
+        }, []);
+        return $results;
     }
 
     protected function saveToDb($data)
     {
+        $data = array_reduce($data, function ($acc, $cur) {
+            $acc[$cur['id']] = $cur;
+            return $acc;
+        }, []);
         $repo = $this->em->getRepository(Post::class);
+        $qb = $repo->createQueryBuilder('p');
+        $dbIdList = $qb
+            ->select('p.postid')
+            ->where(
+                $qb->expr()->in('p.postid', ':idList')
+            )
+            ->setParameters(
+                array(
+                    'idList' => array_keys($data),
+                )
+            )
+            ->getQuery()
+            ->getResult()
+        ;
+        $dbIdList = array_column($dbIdList, 'postid', 'postid');
+        $data = array_diff_key($data, $dbIdList);
         $count = 0;
         foreach ($data as $resPost) {
-            if (!$repo->findOneBy(['postid' => $resPost['id']])) {
-                $count++;
-                $post = new Post();
-                $post->setSubject($resPost['subject'] ?? '');
-                $post->setPostid($resPost['id'] ?? 0);
-                $post->setExamineStatus($resPost['examine_status'] ?? 0);
-                $post->setReplies($resPost['replies'] ?? 0);
-                $post->setAuthor($resPost['author'] ?? '');
-                $post->setIdate(new \DateTime($resPost['idate']));
-                $post->setNdate(new \DateTime($resPost['ndate']));
-                $this->em->persist($post);
-            }
+            $count++;
+            $post = new Post();
+            $post->setSubject($resPost['subject'] ?? '');
+            $post->setPostid($resPost['id'] ?? 0);
+            $post->setExamineStatus($resPost['examine_status'] ?? 0);
+            $post->setReplies($resPost['replies'] ?? 0);
+            $post->setAuthor($resPost['author'] ?? '');
+            $post->setIdate(new \DateTime($resPost['idate']));
+            $post->setNdate(new \DateTime($resPost['ndate']));
+            $this->em->persist($post);
         }
-        $this->em->flush();
+        if ($count) {
+            $this->em->flush();
+        }
         $this->io->note('导入'.$count.'条数据');
     }
 }
